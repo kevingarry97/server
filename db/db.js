983469328bd55fa249bad77feda36ed3a1e73e1e
kevingarry97 <kevingarry97@gmail.com>
2020-07-18 12:07:02 -0700

    Modified

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/tiendra', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+mongoose.connect('mongodb+srv://kevin_garry:Ndahiriwe123@cluster0.ndrrj.mongodb.net/tienda?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
     .then(() => console.log('Connected to mongoDB...'))
     .catch(err => console.log('Could not connect to MongoDB...', err))
 
