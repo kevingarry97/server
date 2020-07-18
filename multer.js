d5efe2d9654fd3c93a4a1c6264d2b18c74da378b
kevingarry97 <kevingarry97@gmail.com>
2020-07-18 10:06:58 -0700

    Added right

--- multer.js
+++ multer.js
@@ -17,7 +17,7 @@ const fileFilter = (req, file, cb) => {
     cb({ message: 'Unsupported File format' }, false)
 }
 
-const upload = multer({ storage: storage, fileFilter: fileFilter, limits: { fieldSize: 10024 * 10024 } })
+const upload = multer({ storage: storage, fileFilter: fileFilter })
 
 module.exports = upload;
 
