b8e5e73a923b2a6683584318cdfc4ed482fe3e3e
kevingarry97 <kevingarry97@gmail.com>
2020-07-18 15:34:29 -0700

    Worked!

--- index.js
+++ index.js
@@ -31,8 +31,12 @@ app.use(cors({credentials: true, origin: 'https://tienda-appl.herokuapp.com'}))
 
 app.use(helmet())
 app.use(compression())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use( bodyParser.json({limit: '50mb'}) );
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+  parameterLimit:50000
+}));
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
