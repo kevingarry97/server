cc99cc1a6b603577127152775853eb7ae8a4f758
kevingarry97 <kevingarry97@gmail.com>
2020-07-18 06:24:12 -0700

    Worked

--- index.js
+++ index.js
@@ -27,7 +27,14 @@ if(!config.get("jwtPrivatekey")) {
 }
 
 // In-Built Middlewares
-app.use(cors({credentials: true, origin: 'https://tienda-appl.herokuapp.com'}))
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+  next()
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
