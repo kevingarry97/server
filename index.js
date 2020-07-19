1ab3caefffc4986e8c31120c782f313b7daf6dab
kevingarry97 <kevingarry97@gmail.com>
2020-07-19 11:11:25 -0700

    Added it

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ if(!config.get("jwtPrivatekey")) {
 
 // In-Built Middlewares
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://tienda-appl.herokuapp.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://tienda-appl.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
