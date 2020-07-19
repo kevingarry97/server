2600f9227ea360630606d8d05fa66c286d35d7bd
kevingarry97 <kevingarry97@gmail.com>
2020-07-19 12:46:15 -0700

    Pushed

--- routes/images.js
+++ routes/images.js
@@ -17,44 +17,28 @@ router.get('/product/:id', async (req, res) => {
 })
 
 router.post('/upload-images', upload.array('files'), async (req, res) => {
+    const uploader = async (path) => await cloudinary.uploads(path, 'Images')
+
     const urls = []
-    let files = req.files
-    const url = req.protocol + '://' + req.get('host')
+    const files = req.files;
+
+    const product = await Product.findById(req.body.productId)
+    if(!product) return res.status(400).send(`Can't find product`)
 
     for(const file of files) {
         const { path } = file
 
-        urls.push(url + '/image/' +path)
+        const newPath = await uploader(path)
+
+        urls.push(newPath)
     }
 
-    let image = new Image({
-        image: urls
+    let images = new Image({
+        image: urls.map(item => item['url']),
+        product: product
     })
-
-    image = await image.save()
-    res.send(image);
-    // const uploader = async (path) => await cloudinary.uploads(path, 'Images')
-
-    // const urls = []
-    // const files = req.files;
-
-    // const product = await Product.findById(req.body.productId)
-    // if(!product) return res.status(400).send(`Can't find product`)
-
-    // for(const file of files) {
-    //     const { path } = file
-
-    //     const newPath = await uploader(path)
-
-    //     urls.push(newPath)
-    // }
-
-    // let images = new Image({
-    //     image: urls.map(item => item['url']),
-    //     product: product
-    // })
-    // images = await images.save()
-    // res.status(200).send(images)
+    images = await images.save()
+    res.status(200).send(images)
 })
 
 module.exports = router;
