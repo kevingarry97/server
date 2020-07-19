b4654029865c3bc80ec8337aa242fef7a42ca140
kevingarry97 <kevingarry97@gmail.com>
2020-07-19 07:52:26 -0700

    Modified

--- routes/images.js
+++ routes/images.js
@@ -2,7 +2,6 @@ const express = require('express')
 const upload = require('../multer')
 const mongoose = require('mongoose')
 const { Product } = require('../model/product')
-const cloudinary = require('../cloudinary');
 const { Image } = require('../model/image')
 const router = express.Router()
 
@@ -17,10 +16,10 @@ router.get('/product/:id', async (req, res) => {
 })
 
 router.post('/upload-images', upload.array('files'), async (req, res) => {
-    const uploader = async (path) => await cloudinary.uploads(path, 'Images')
 
     const urls = []
     const files = req.files;
+    const url = req.protocol + '://' + req.get('host')
 
     const product = await Product.findById(req.body.productId)
     if(!product) return res.status(400).send(`Can't find product`)
@@ -28,15 +27,14 @@ router.post('/upload-images', upload.array('files'), async (req, res) => {
     for(const file of files) {
         const { path } = file
 
-        const newPath = await uploader(path)
-
-        urls.push(newPath)
+        urls.push(url + '/image/' +path)
     }
 
     let images = new Image({
-        image: urls.map(item => item['url']),
+        image: urls,
         product: product
     })
+    
     images = await images.save()
     res.status(200).send(images)
 })
