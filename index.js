740b7f2867aef5d68d3cf51281866e45cc623a1b
kevingarry97 <kevingarry97@gmail.com>
2020-07-20 16:10:45 -0700

    Modified

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if(!config.get("jwtPrivatekey")) {
 //   'https://tienda-appl.herokuapp.com', 'http://localhost:4200'
 // ], credentials: true}));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
