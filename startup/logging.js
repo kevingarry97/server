27061a595764ee4b5723d649c9005ed42d1f0d06
kevingarry97 <kevingarry97@gmail.com>
2020-07-24 03:29:27 -0700

    Published

--- startup/logging.js
+++ startup/logging.js
@@ -1,12 +1,13 @@
 const winston = require('winston');
-require('winston-mongodb')
+// require('winston-mongodb')
 require('express-async-errors')
 
 module.exports = function() {
     
-    winston.exceptions.handle(new winston.transports.File({
-        filename: 'uncaughtExceptions.log'
-    }))
+    winston.exceptions.handle(
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
+        new winston.transports.File({filename: 'uncaughtExceptions.log'})
+    )
     
     process.on('unhandledRejection', (ex) => {
         throw ex;
