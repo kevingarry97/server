0ce68913d640a2470397ce7c3fc49bb79c3ba799
kevingarry97 <kevingarry97@gmail.com>
2020-07-24 03:40:53 -0700

    Colorize

--- startup/logging.js
+++ startup/logging.js
@@ -5,6 +5,7 @@ require('express-async-errors')
 module.exports = function() {
     
     winston.exceptions.handle(
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
         new winston.transports.File({filename: 'uncaughtExceptions.log'})
     )
     
